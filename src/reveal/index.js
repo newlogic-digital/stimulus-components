38270b059c99351bcb903eb5ada0b3f5afb18ab7
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2026-04-22 08:53:13 +0200

    feat: add configurable values and clear option to Reveal controller

--- src/reveal/index.js
+++ src/reveal/index.js
@@ -4,19 +4,37 @@ import { dataset } from '@newlogic-digital/utils-js'
 export class Reveal extends Controller {
   static targets = ['item']
 
+  static values = {
+    threshold: {
+      type: Number,
+      default: 0.1,
+    },
+    intersectionRatio: {
+      type: Number,
+      default: 0.1,
+    },
+    clear: {
+      type: Boolean,
+      default: false,
+    },
+  }
+
   intersectionObserver() {
     this.observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
-        if (entry.intersectionRatio > 0.1 && !entry.target.hasAttribute('data-in')) {
+        if (entry.intersectionRatio > this.intersectionRatio && !entry.target.hasAttribute('data-in')) {
           entry.target.setAttribute('data-in', '')
 
           if (entry.target.dataset.lazyController) {
             dataset(entry.target, 'controller').add(entry.target.dataset.lazyController)
           }
         }
+        else if (this.clearValue) {
+          entry.target.removeAttribute('data-in')
+        }
       })
     }, {
-      threshold: 0.1,
+      threshold: this.thresholdValue,
     })
   }
 
