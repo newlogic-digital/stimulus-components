f47623ae8270f1d992b539aec96e54825e75a312
petr-adamek <petr.adamek@newlogic.cz>
2026-08-11 11:57:51 +0200

    feat: add recaptcha-lazy to the node package directly.

--- src/recaptcha/index.js
+++ src/recaptcha/index.js
@@ -1,5 +1,5 @@
 import { Controller } from '@hotwired/stimulus'
-import { importScript } from '@newlogic-digital/utils-js'
+import { importScript, dataset } from '@newlogic-digital/utils-js'
 
 export class ReCaptcha extends Controller {
   static values = {
@@ -21,5 +21,59 @@ export class ReCaptcha extends Controller {
       this.element.gtoken.value = await window.grecaptcha.enterprise.execute(this.apiValue, { action: this.actionValue ?? 'form' })
       this.element.dispatchEvent(new CustomEvent('submit', { cancelable: true, detail: { recaptchaExecuted: true } }))
     })
+  }
+}
+
+export class ReCaptchaLazy extends Controller {
+  static values = {
+    recaptchaClass: String,
+    recaptchaLazyClass: String,
+  }
+
+  get recaptchaClass() {
+    return this.recaptchaClassValue || this.identifier.replace(/-lazy$/, '')
+  }
+
+  get recaptchaLazyClass() {
+    return this.recaptchaLazyClassValue || this.identifier
+  }
+
+  connect() {
+    if (this.element.dataset.controller.split(' ').indexOf(this.recaptchaClass) > -1) {
+      return
+    }
+    this.function = this.loadRecaptcha.bind(this)
+
+    this.element.addEventListener('focusin', this.function, { once: true })
+
+    this._observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          this.loadRecaptcha()
+        }
+      })
+    }, {
+      root: null,
+      rootMargin: `${window.innerHeight}px`,
+      threshold: 0,
+    })
+
+    this._observer.observe(this.element)
+  }
+
+  disconnect() {
+    if (typeof this._observer !== 'undefined') {
+      this._observer.disconnect()
+    }
+  }
+
+  loadRecaptcha() {
+    if (typeof this._observer !== 'undefined') {
+      this._observer.disconnect()
+    }
+
+    this.element.removeEventListener('focusin', this.function)
+    dataset(this.element, 'controller').add(this.recaptchaClass)
+    dataset(this.element, 'controller').remove(this.recaptchaLazyClass)
   }
 }
